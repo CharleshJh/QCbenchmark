71591a1c7329e420592c5a5ba26abaab42a2d1be
Chilsan <joe00529@gmail.com>
2020-05-11 18:53:32 +0800

    fix error

--- rep2.py
+++ rep2.py
@@ -25,28 +25,36 @@ for f in inDir:
         path = path.replace('.txt', '.real')
         
         with open(path, 'w') as writingFile:
-            for f1 in fileLines:
-                if f1 == fileLines[0]:
+            for f1 in range(len(fileLines)):
+                if f1 == 0:
                     writingFile.write('.version 1.0\n')
-                    writingFile.write('.numvar '+ f1)
+                    writingFile.write('.numvar '+ fileLines[f1])
                     varLine = ".variables"
-                    for x in range(int(f1)):
+                    for x in range(int(fileLines[f1])):
                         varLine = varLine + " q" + str(x)
                     writingFile.write(varLine + '\n')
-                    writingFile.write(varLine.replace('.variables', '.inputs') + '\n')
-                    writingFile.write(varLine.replace('.variables', '.outputs') + '\n')
+                    varLine = ".constant "
+                    for x in range(int(fileLines[f1])):
+                        varLine = varLine + "0"
+                    writingFile.write(varLine + '\n')
+                    varLine = ".garbage "
+                    for x in range(int(fileLines[f1])):
+                        varLine = varLine + "-"
+                    writingFile.write(varLine + '\n')
                     writingFile.write('.begin\n')
                 else:
-                    aLine = f1.split()
+                    aLine = fileLines[f1].split()
                     if aLine[1] == 'h' or aLine[1] == 'z':
                         writingFile.write(aLine[1] + '1 q' + aLine[2] + '\n')
                     elif aLine[1] == 'cz':
-                        writingFile.write('z2 q' + aLine[2] + ' ' + aLine[3] + '\n')
+                        writingFile.write('z2 q' + aLine[2] + ' q' + aLine[3] + '\n')
                     elif aLine[1] == 'x_1_2':
                         writingFile.write('x1 q' + aLine[2] + '\n')
                     elif aLine[1] == 'y_1_2':
                         writingFile.write('y1 q' + aLine[2] + '\n')
                     elif aLine[1] == 't':
                         writingFile.write('q1:4 q' + aLine[2] + '\n')
+                if f1 == (len(fileLines) - 1):
+                    writingFile.write('.end\n')
             writingFile.close()
 
