7d5b460dced547f7ab94ffdaef319d47f2d716d4
Chilsan <joe00529@gmail.com>
2020-05-13 18:17:46 +0800

    finally fix

--- rep2.py
+++ rep2.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, isdir, join
 
 mypath = sys.argv[1]
-layers = sys.argv[2]
+layers = int(sys.argv[2])
 os.chdir(mypath)
 if not os.path.exists('out'):
     os.mkdir('out')
@@ -26,7 +26,7 @@ for f in inDir:
         path = path.replace('.txt', '.real')
         
         with open(path, 'w') as writingFile:
-            while f1 < len(fileLines):
+            for f1 in range(len(fileLines)):
                 if f1 == 0:
                     writingFile.write('.version 1.0\n')
                     writingFile.write('.numvars '+ fileLines[f1])
@@ -56,7 +56,7 @@ for f in inDir:
                             writingFile.write('y1 q' + aLine[2] + '\n')
                         elif aLine[1] == 't':
                             writingFile.write('q1:4 q' + aLine[2] + '\n')
-                if f1 == len(fileLines - 1):
+                if f1 == len(fileLines) - 1:
                     writingFile.write('.end\n')
             writingFile.close()
 
